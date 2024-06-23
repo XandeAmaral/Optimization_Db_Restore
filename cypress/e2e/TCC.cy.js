7540eb100df4f51acb94fadde0cd0a6393a340aa
Alexandre Amaral <alexandrecamaral22@gmail.com>
2024-06-23 00:49:38 -0300

    FEAT - implatado para o ss e exibicao das medias

--- cypress/e2e/TCC.cy.js
+++ cypress/e2e/TCC.cy.js
@@ -2,58 +2,68 @@ describe('Data analysis', () => {
   let inicio;
   let fim;
 
-  it.only('Restore from Backup', () => {
-    cy.step('Drop snapshots');
-    cy.dropSnapshot();
+  Cypress._.times(1, () => {
+    context('Testes performance inserindo dados', () => {
+      it('Restore from Backup', () => {
+        cy.step('Drop snapshots');
+        cy.dropSnapshot();
 
-    cy.step('Manipulim database');
-    // cy.insertDatas();
-    // cy.insertDatas_2();
-    cy.insertDatas_3();
+        cy.step('Manipulim database');
+        // cy.insertDatas();
+        // cy.insertDatas_2();
+        cy.insertDatas_3();
 
-    cy.step('Restore backup');
-    cy.wrap('').then(() => {
-      inicio = performance.now();
-    });
-    cy.restoreBackup();
-    cy.wrap('').then(() => {
-      fim = performance.now();
-      const result = fim - inicio;
+        cy.step('Restore backup');
+        cy.wrap('', { log: false }).then(() => {
+          inicio = performance.now();
+        });
 
-      cy.task('writeTiming', { result, type: 'backup' }).then((response) => {
-        if (response.error) {
-          console.error(response.error);
-        } else {
-          console.log('Tempo registrado com sucesso');
-        }
+        cy.restoreBackup();
+
+        cy.wrap('').then(() => {
+          fim = performance.now();
+          const result = fim - inicio;
+
+          cy.task('writeTiming', { result, type: 'backup' }).then((response) => {
+            if (response.error) {
+              console.error(response.error);
+            } else {
+              console.log('Tempo registrado com sucesso');
+              console.log(`A média dos tempos de ${response.type} é de ${response.media} milissegundos`);
+            }
+          });
+        });
       });
-    });
-  });
 
-  it('Restore from snapshots', () => {
-    cy.step('Create snapshots');
-    cy.createSnapshots();
+      it('Restore from snapshots', () => {
+        cy.step('Create snapshots');
+        cy.createSnapshots();
 
-    cy.step('Manipulim database');
-    // cy.insertDatas();
-    // cy.insertDatas_2();
-    cy.insertDatas_3();
+        cy.step('Manipulim database');
+        // cy.insertDatas();
+        // cy.insertDatas_2();
+        cy.insertDatas_3();
 
-    cy.step('Restore snapshot');
-    cy.wrap('', c).then(() => {
-      console.time('timeSnapshot');
-    });
-    cy.restoreSnapshot();
-    cy.wrap('', { log: false }).then(() => {
-      fim = performance.now();
-      const result = fim - inicio;
+        cy.step('Restore snapshot');
+        cy.wrap('', { log: false }).then(() => {
+          inicio = performance.now();
+        });
 
-      cy.task('writeTiming', { result, type: 'snapshot' }).then((response) => {
-        if (response.error) {
-          console.error(response.error);
-        } else {
-          console.log('Tempo registrado com sucesso');
-        }
+        cy.restoreSnapshot();
+
+        cy.wrap('', { log: false }).then(() => {
+          fim = performance.now();
+          const result = fim - inicio;
+
+          cy.task('writeTiming', { result, type: 'snapshot' }).then((response) => {
+            if (response.error) {
+              console.error(response.error);
+            } else {
+              console.log('Tempo registrado com sucesso');
+              console.log(`A média dos tempos de ${response.type} é de ${response.media} milissegundos`);
+            }
+          });
+        });
       });
     });
   });
